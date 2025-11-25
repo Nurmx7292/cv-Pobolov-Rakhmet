fdd0a2317c3d0d420e5543286bf567349bcbddd0
pobolovInnowise <tymur.pobolov@innowise.com>
2025-11-25 15:22:42 +0100

    feat(features/auth): add login API

---
+++ src/features/auth/login/api/loginMutation.ts
@@ -0,0 +1,16 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_QUERY = gql`
+    query Login($email: String!, $password: String!) {
+        login(auth: { email: $email, password: $password }) {
+            access_token
+            refresh_token
+            user {
+                id
+                email
+                role
+            }
+        }
+    }
+`;
+
