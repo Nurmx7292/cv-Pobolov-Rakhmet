9941cd31e8824e57fd0ac41c0585c3f8f7af30a6
pobolovInnowise <tymur.pobolov@innowise.com>
2025-11-25 15:28:03 +0100

    feat(entities/user): add user model types

---
+++ src/entities/user/model/types.ts
@@ -0,0 +1,14 @@
+export interface UserProfile {
+    first_name: string | null;
+    last_name: string | null;
+}
+
+export interface User {
+    id: string;
+    email: string;
+    role: string;
+    department_name: string | null;
+    position_name: string | null;
+    profile: UserProfile | null;
+}
+
