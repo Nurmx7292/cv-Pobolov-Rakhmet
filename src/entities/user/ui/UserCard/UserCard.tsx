57ae9ac745b4d2c1900ae6bc821ac6eb0fce4b72
pobolovInnowise <tymur.pobolov@innowise.com>
2025-11-25 17:09:51 +0100

    style(entities/user): update component styles

--- src/entities/user/ui/UserCard/UserCard.tsx
+++ src/entities/user/ui/UserCard/UserCard.tsx
@@ -6,17 +6,27 @@ interface Props {
 }
 
 export const UserCard = ({ user }: Props) => {
-    const firstName = user.profile?.first_name ?? "";
-    const lastName = user.profile?.last_name ?? "";
-    const fullName = [firstName, lastName].filter(Boolean).join(" ") || "No name";
+    const firstName = user?.profile?.first_name ?? "";
+    const lastName = user?.profile?.last_name ?? "";
+    const email = user?.email ?? "";
+    const role = user?.role ?? "";
+    const department_name = user?.department_name ?? "";
+    const position_name = user?.position_name ?? "";
+
+
+    const avatarAltText = firstName ? firstName.toUpperCase().split('')[0] : (lastName ? lastName.toUpperCase().split('')[0] : email.toUpperCase().split('')[0])
+
+
 
     return (
         <article className={styles.card}>
-            <div className={styles.name}>{fullName}</div>
-            <div className={styles.meta}>{user.email}</div>
-            <div className={styles.meta}>{user.role}</div>
-            <div className={styles.meta}>{user.department_name || "No department"}</div>
-            <div className={styles.meta}>{user.position_name || "No position"}</div>
+            <div className={styles.avatar}><p>{avatarAltText}</p></div>
+            <div className={styles.firstName}>{firstName}</div>
+            <div className={styles.lastName}>{lastName}</div>
+            <div className={styles.email}>{email}</div>
+            <div className={styles.role}>{role}</div>
+            <div className={styles.departmentName}>{department_name || "No department"}</div>
+            <div className={styles.positionName}>{position_name || "No position"}</div>
         </article>
     );
 };
