195fd28e66530aff51cc037db1dd72b22514aa3d
pobolovInnowise <tymur.pobolov@innowise.com>
2025-11-26 13:12:35 +0100

    refactor(entities/user): normalize user data mapping in UserList

--- src/widgets/users/ui/UserList/UserList.tsx
+++ src/widgets/users/ui/UserList/UserList.tsx
@@ -3,25 +3,30 @@ import { UserCard, useUsersQuery } from "@entities/user";
 export const UserList = () => {
     const { data, loading, error } = useUsersQuery();
 
-    if (loading) {
-        return <p>Loading users...</p>;
-    }
+    if (loading) return <p>Loading users...</p>;
+    if (error) return <p>Failed to load users: {error.message}</p>;
 
-    if (error) {
-        return <p>Failed to load users: {error.message}</p>;
-    }
+    const users = data.users.map(user=>{
+        return {
+            id: user.id,
+            email: user.email ? user.email : "",
+            role: user.role,
+            department_name: user.department_name,
+            position_name: user.position_name,
+            first_name: user.profile.first_name ? user.profile.first_name : "",
+            last_name: user.profile.last_name ? user.profile.last_name : "",
+            avatar: user.profile.avatar
+        }
+    });
 
     return (
         <div>
             <h2>Users</h2>
-           {/*<div> columns names  </div>*/}
-           {/* <div>show info about logged in user</div>*/}
             <div>
-                {data?.users.map((user) => (
+                {users.map((user) => (
                     <UserCard key={user.id} user={user} />
                 ))}
             </div>
         </div>
     );
 };
-
