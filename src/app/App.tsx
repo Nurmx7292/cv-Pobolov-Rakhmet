188866b980db11d7aa314b9a2bb4458822db9821
Nurmx7292 <raxmetnur04@gmail.com>
2025-11-26 12:50:42 +0100

    chore(app): integrate theme provider in App shell

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,14 @@
 import { WithApolloProvider } from "./providers/WithApolloProvider.tsx";
+import { WithThemeProvider } from "./providers/WithThemeProvider.tsx";
 import { AppRouter } from "./providers/router/AppRouter.tsx";
 
 export const App = () => {
     return (
-        <WithApolloProvider>
-            <AppRouter />
-        </WithApolloProvider>
+        <WithThemeProvider>
+            <WithApolloProvider>
+                <AppRouter />
+            </WithApolloProvider>
+        </WithThemeProvider>
     );
 };
 
