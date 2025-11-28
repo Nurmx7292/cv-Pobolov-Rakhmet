4b25a904552404d296fa733bf2938c3396fbf3a7
pobolovInnowise <tymur.pobolov@innowise.com>
2025-11-28 13:58:26 +0100

    feat: add local state and profile inputs

--- src/widgets/users/ui/UserProfile/UserProfile.tsx
+++ src/widgets/users/ui/UserProfile/UserProfile.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from "./UserProfile.module.css";
 import {useParams} from "react-router-dom";
 import {useQuery} from "@apollo/client/react";
 import {GET_USER_BY_ID_QUERY} from "@widgets/users/api/getUserByIdQuery";
+import TextField from '@mui/material/TextField';
 
 const UserProfile = () => {
+    const [firstNameInputValue, setFirstNameInputValue] = useState('');
+    const [lastNameInputValue, setLastNameInputValue] = useState('');
+
     const {userId} = useParams<{ userId: string }>();
 
     const {loading, error, data} = useQuery(GET_USER_BY_ID_QUERY, {
@@ -12,12 +16,19 @@ const UserProfile = () => {
         skip: !userId,
     });
 
+    const user = data?.user;
+    const profile = user?.profile;
+
+    useEffect(() => {
+        if (!user) return;
+
+        setFirstNameInputValue(profile?.first_name || '');
+        setLastNameInputValue(profile?.last_name || '');
+    }, [user, profile]);
+
     if (loading) return <div>Загрузка профиля...</div>;
     if (error) return <div>Ошибка загрузки: {error.message}</div>;
-    if (!data?.user) return <div>Пользователь не найден</div>;
-
-    const user = data.user;
-    const profile = user.profile;
+    if (!user) return <div>Пользователь не найден</div>;
 
     const created_at = user.created_at;
     const email = user.email;
@@ -32,9 +43,27 @@ const UserProfile = () => {
 
     return (
         <div className={styles.userProfile}>
-            <div>{profile?.first_name} {profile?.last_name}</div>
+            <div>{firstNameInputValue} {lastNameInputValue}</div>
             <div>{email}</div>
             <div>A member since {memberSinceString}</div>
+
+            <TextField
+                className={styles.input}
+                label="First Name"
+                variant="outlined"
+                value={firstNameInputValue}
+                onChange={e => setFirstNameInputValue(e.target.value)}
+                InputLabelProps={{shrink: firstNameInputValue !== ''}}
+            />
+
+            <TextField
+                className={styles.input}
+                label="Last Name"
+                variant="outlined"
+                value={lastNameInputValue}
+                onChange={e => setLastNameInputValue(e.target.value)}
+                InputLabelProps={{shrink: lastNameInputValue !== ''}}
+            />
         </div>
     );
 };
