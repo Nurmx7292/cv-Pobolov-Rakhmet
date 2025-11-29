9b2e9e1034b0ecd0c9003b052c64d8657c984596
pobolovInnowise <tymur.pobolov@innowise.com>
2025-11-29 13:42:05 +0100

    feat: add user label to Sidebar user menu and rename UserProfile to UserProfileNav in UserMenu folder

--- src/widgets/sidebar/ui/Sidebar/UserProfile/UserProfileNav.tsx
+++ src/widgets/sidebar/ui/Sidebar/UserProfile/UserProfileNav.tsx
@@ -1,14 +1,34 @@
 import { useState } from "react";
 import { Box, Avatar, Typography, useTheme } from "@mui/material";
 import { UserMenu } from "./UserMenu/UserMenu";
-import styles from "./UserProfile.module.css";
+import styles from "./UserProfileNav.module.css";
+import { useQuery } from "@apollo/client/react";
+import {GET_USER_BY_ID_QUERY} from "@widgets/users/api/getUserByIdQuery.ts";
+
 
 
 interface UserProfileProps {
     isCollapsed: boolean;
 }
 
-export const UserProfile = ({ isCollapsed }: UserProfileProps) => {
+export const UserProfileNav = ({ isCollapsed }: UserProfileProps) => {
+
+    const currentUserId = localStorage.getItem("currentUserId");
+
+    const {loading, error, data} = useQuery(GET_USER_BY_ID_QUERY, {
+        variables: {id: currentUserId},
+        skip: !currentUserId,
+        notifyOnNetworkStatusChange: false
+    });
+
+    const user = data?.user;
+    const profile = user?.profile;
+
+    const email = user?.email ?? "";
+    const firstName = profile?.first_name ?? "";
+    const lastName = profile?.last_name ?? "";
+    const avatar = profile?.avatar;
+
     const theme = useTheme();
     const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
     const open = Boolean(anchorEl);
@@ -56,7 +76,7 @@ export const UserProfile = ({ isCollapsed }: UserProfileProps) => {
                             transition: "opacity 0.2s ease, max-width 0.2s ease",
                         }}
                     >
-                        testacc5@gmail.com
+                        {(firstName||lastName) ? `${firstName} ${lastName}`: email}
                     </Typography>
                 </Box>
             </Box>    
