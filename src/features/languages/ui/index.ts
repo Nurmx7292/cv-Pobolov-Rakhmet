1ff3e00a332d5ae6dc7ea2c3ba32fb9d450e7f36
Nurmx7292 <raxmetnur04@gmail.com>
2025-11-30 17:53:14 +0100

    feat(languages/features): add UI components for language selection and display

---
+++ src/features/languages/ui/index.ts
@@ -0,0 +1,4 @@
+export { LanguageSelect } from "./LanguageSelect/LanguageSelect";
+export { LanguageProficiencySelect } from "./LanguageProficiencySelect/LanguageProficiencySelect";
+export { LanguageProficiency } from "./LanguageProficiency/LanguageProficiency";
+
