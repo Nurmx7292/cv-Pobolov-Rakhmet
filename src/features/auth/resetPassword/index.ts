74a1a237c7d1dd75832b88c65b5d5ba30008124e
pobolovInnowise <tymur.pobolov@innowise.com>
2025-12-01 13:46:33 +0100

    feat(auth): add routing and integrate forgot/reset password, implement reset password features

---
+++ src/features/auth/resetPassword/index.ts
@@ -0,0 +1,3 @@
+export { ResetPasswordForm } from "./ui/ResetPasswordForm/ResetPasswordForm";
+export { useResetPassword } from "./api/useResetPassword";
+
--- src/shared/api/apolloClient.ts
+++ src/shared/api/apolloClient.ts
@@ -1,6 +1,11 @@
-import { ApolloClient, HttpLink, InMemoryCache, from } from "@apollo/client";
+import {
+    ApolloClient,
+    HttpLink,
+    InMemoryCache,
+    from,
+    ApolloLink,
+} from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
-import { setContext } from "@apollo/client/link/context";
 import { env } from "@shared/config/env";
 import { tokenStorage } from "@shared/lib/tokenStorage";
 
@@ -8,57 +13,70 @@ const httpLink = new HttpLink({
     uri: env.GRAPHQL_API_URL,
 });
 
-const authLink = setContext((_, { headers }) => {
+const authLink = new ApolloLink((operation, forward) => {
     const token = tokenStorage.getAccessToken();
 
-    return {
+    const operationContext = operation.getContext();
+    const contextHeaders = operationContext.headers ?? {};
+
+    operation.setContext(({ headers = {} }) => ({
         headers: {
             ...headers,
-            Authorization: token ? `Bearer ${token}` : undefined,
+            ...contextHeaders,
+            Authorization:
+                contextHeaders.Authorization ??
+                (token ? `Bearer ${token}` : undefined),
         },
-    };
+    }));
+
+    return forward(operation);
 });
 
-const errorLink = onError(({ graphQLErrors, networkError, operation, forward }) => {
-    if (graphQLErrors) {
-        graphQLErrors.forEach((graphQLError) => {
-            console.error("GraphQL Error:", {
-                message: graphQLError.message,
-                locations: graphQLError.locations,
-                path: graphQLError.path,
-                extensions: graphQLError.extensions,
+const errorLink = onError(
+    ({ graphQLErrors, networkError, operation }) => {
+        if (graphQLErrors) {
+            graphQLErrors.forEach((graphQLError) => {
+                console.error("GraphQL Error:", {
+                    message: graphQLError.message,
+                    locations: graphQLError.locations,
+                    path: graphQLError.path,
+                    extensions: graphQLError.extensions,
+                    operation: operation.operationName,
+                    variables: operation.variables,
+                });
+
+                if (
+                    graphQLError.message === "Unauthorized" ||
+                    graphQLError.extensions?.code === "UNAUTHENTICATED"
+                ) {
+                    tokenStorage.clearTokens();
+                }
+            });
+        }
+
+        if (networkError) {
+            console.error("Network Error:", {
+                message: networkError.message,
+                statusCode:
+                    "statusCode" in networkError
+                        ? networkError.statusCode
+                        : undefined,
                 operation: operation.operationName,
                 variables: operation.variables,
             });
-            
+
             if (
-                graphQLError.message === "Unauthorized" ||
-                graphQLError.extensions?.code === "UNAUTHENTICATED"
+                "statusCode" in networkError &&
+                (networkError.statusCode === 401 ||
+                    networkError.statusCode === 403)
             ) {
                 tokenStorage.clearTokens();
             }
-        });
-    }
-
-    if (networkError) {
-        console.error("Network Error:", {
-            message: networkError.message,
-            statusCode: "statusCode" in networkError ? networkError.statusCode : undefined,
-            operation: operation.operationName,
-            variables: operation.variables,
-        });
-        
-        if (
-            "statusCode" in networkError &&
-            (networkError.statusCode === 401 || networkError.statusCode === 403)
-        ) {
-            tokenStorage.clearTokens();
         }
     }
-});
+);
 
 export const apolloClient = new ApolloClient({
     link: from([errorLink, authLink, httpLink]),
     cache: new InMemoryCache(),
 });
-
